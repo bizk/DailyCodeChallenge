d1dbe3feeda8aed75c7aaa1361703bbf482ae41a
Carlos Santiago Yanzon <santiago.yanzon1999@gmail.com>
2019-03-09 20:41:22 +0100

    Challenge #4 - HARD
    No fue para nada dificil o yo lo hice mal je

---
+++ Daily_coding_problem_4_hard.py
@@ -0,0 +1,33 @@
+def buscar(arreglo):
+    ch = []
+    arreglo.sort()
+    for x in arreglo:
+        if x > 0:
+            ch.append(x)
+    ch = list(set(ch))
+    lench = len(ch)
+    minimo = 0
+    if lench > 1:
+        for i in range(1, lench):
+            ex = ch[i - 1] + 1
+            if ex != ch[i]:
+                if minimo != 0:
+                    minimo = min(minimo, ex)
+                else:
+                    minimo = ch[i-1]+1
+
+    print("Este es el numero que falta: ", minimo)
+
+
+arr = []
+print("Ingrese la cadena de caracteres: \n")
+while True:
+    ln = input()
+    try:
+        ln = int(ln)
+    except:
+        break
+    arr.append(ln)
+
+print("SALIO")
+buscar(arr)
